0f8fbd03a000ecfc83e9b6cd4137d56411404b78
QiuSun <13038011192@163.com>
2017-08-13 07:49:48 +0800

    head.cpp

---
+++ head.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <map>
+#include <complex>
+using namespace std;
+typedef long long ll;
+typedef long double db;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+#define de(x) cout << #x << "=" << x << endl
+#define rep(i,a,b) for(int i=a;i<(b);++i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define E 1e-6;
+void open()
+{
+    freopen("data.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+}
+const int maxn = 101010;
